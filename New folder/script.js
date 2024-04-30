126463cc495f025a7a45b24aca14710ebf8c7e53
RedonMehmeti <123652604+RedonMehmeti@users.noreply.github.com>
2024-04-30 08:02:19 -0700

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,15 @@
+// let regex1 = /[a-c]/
+// let regex2 = new RegExp('[a-c]')
+
+// let emri = "jcoders"
+// console.log(regex1.exec(emri))
+
+
+// let text = "London, Spain, Paris";
+// let result = text.match("ain");
+// console.log(result)
+
+
+let text = "E hwnw";
+let result = text.replaceAll("w", "ë");
+console.log(result)
